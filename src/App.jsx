36a05cded68d6231f843ee10e7aae70eb4448138
Patricio Gabriel Maseda <pmaseda@devspark.com>
2017-03-16 17:11:29 -0300

    State update falling piece action.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 // Components.
 import Main from './components/Main/Main';
 
-
 class App extends Component {
 
   constructor(props) {
@@ -13,6 +12,19 @@ class App extends Component {
       fallingPieceX: 10,
       fallingPieceY: 50
     };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      let fallingPieceY = this.state.fallingPieceY - 5;
+      if (fallingPieceY < 0) {
+        fallingPieceY = 50;
+      }
+
+      this.setState({
+        fallingPieceY: fallingPieceY
+      });
+    }, 1000);
   }
 
   render() {
