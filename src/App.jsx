c2bc537af450986ebb0bdebb66a6596dc53f759d
Patricio Gabriel Maseda <pmaseda@devspark.com>
2017-03-16 15:52:41 -0300

    Main container created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+// Components.
+import Main from './components/Main/Main';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fallingPieceX: 10,
+      fallingPieceY: 50
+    };
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <Main {...this.state} />
     );
   }
 }
+
 export default App;
