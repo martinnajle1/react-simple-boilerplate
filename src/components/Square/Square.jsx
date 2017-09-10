fea382246d313da05d38d1f54805bdd7a15b315c
martinnajle1 <martin.najle@gmail.com>
2017-09-10 18:02:56 -0300

    Square with classname of color instead of style inline

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {HeightBoard, SizeSquare} from '../../constants.js';
 import square from './square.scss';
+const classNames = require('classnames');
+
 
 
 class Square extends Component {
@@ -10,13 +12,12 @@ class Square extends Component {
   }
 
   render() {
-    return <div className="square" style={{ 
+    return <div className={classNames(this.props.aColor, 'square')} style={{ 
     	left:(this.props.posX*SizeSquare)+'px',  
 	    top:((this.props.base*SizeSquare)-this.props.posY*SizeSquare)+'px', 
 	    height: SizeSquare +'px', 
-	    width: SizeSquare +'px', 
-	    background: this.props.aColor
-	}}/>;
+	    width: SizeSquare +'px'
+	  }}/>;
   }
 }
 
