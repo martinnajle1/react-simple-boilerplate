ff91881c4b9eff0e84ff3027b92fe6e57e5e9bfb
martinnajle1 <martin.najle@gmail.com>
2018-06-11 21:11:44 -0300

    Styled components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Square from '../Square/Square.jsx';
 import Piece from '../Piece/Piece.jsx';
 import {WidthBoard, HeightBoard, SizeSquare} from '../../constants.js';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+const Wrapper = styled.div`
+  border: 2px solid black;
+  border-top: none;
+  position: relative;
+  display: inline-block;
+  height: ${HeightBoard * SizeSquare}px;
+  width: ${WidthBoard * SizeSquare}px;
+  & .animation-leave {
+    opacity: 1;
+  }
+  & .animation-leave.animation-leave-active {
+    opacity: 0.01;
+    transition: opacity 400ms ease-out;
+  }`;
 const Main = (props) => {
   const {piece, fallingPieceX, fallingPieceY, squares} = props;
   const pieces = squares.map(function (square) {
@@ -17,16 +33,14 @@ const Main = (props) => {
     );
   });
   return (
-    <div className="main" style={{
-      'height': `${HeightBoard * SizeSquare}px`,
-      'width': `${WidthBoard * SizeSquare}px`}}>
+    <Wrapper>
       <Piece piece={piece} posX={fallingPieceX} posY={fallingPieceY} />
       <br/>
       <ReactCSSTransitionGroup transitionEnterTimeout={500}
       transitionLeaveTimeout={400} transitionName="animation">
         {pieces}
       </ReactCSSTransitionGroup>
-    </div>
+    </Wrapper>
   );
 };
 Main.propTypes = {
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import {SizeSquare} from '../../constants.js';
-import './square.scss';
-import classNames from 'classnames';
 
 const Square = (props) => {
   const {posX, posY, base, aColor} = props;
+  const stylePosition = {
+    'left': `${posX * SizeSquare}px`,
+    'top': `${(base * SizeSquare) - (posY * SizeSquare)}px`,
+    'height': `${SizeSquare}px`,
+    'width': `${SizeSquare}px`
+  };
+  const StyledSquare = styled.div`
+    border: 3px solid red;
+    width: 80px;
+    height: 80px;
+    position: absolute;
+    background: ${aColor};
+    boxSizing: border-box;
+    border:gray solid 1px;               
+    borderRadius: 3px; 
+    position: absolute; 
+  `;
   return (
-    <div className={classNames(aColor, 'square')} style={{
-      'left': `${posX * SizeSquare}px`,
-      'top': `${(base * SizeSquare) - (posY * SizeSquare)}px`,
-      'height': `${SizeSquare}px`,
-      'width': `${SizeSquare}px`
-    }}/>
+    <StyledSquare style={stylePosition} />
   );
 };
 
